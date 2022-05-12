aa6599ff0f6c7eeff1808c3b31c55a9c89fd3af6
AngelaJiaNan <angelanan12@gmail.com>
2022-05-12 14:31:21 -0700

    finished 4 question.

--- javascript-functions-practice/get-last-element.js
+++ javascript-functions-practice/get-last-element.js
@@ -1,1 +1,7 @@
 /* exported getLastElement */
+function getLastElement(array) {
+  return array[array.length - 1];
+}
+
+// 1. need to figure out the length of the array
+// 2. use index to access the array
