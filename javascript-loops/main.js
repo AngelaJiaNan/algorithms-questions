1731c0c640f4fb5ca4eb9b5ad4597225cff41da8
AngelaJiaNan <angelanan12@gmail.com>
2022-05-23 16:26:50 -0700

    finsihed 2 functions.

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -9,5 +9,25 @@
 function getNumbersToTen() {
   var numbers = [];
   var currentNumber = 1;
-
+  while (currentNumber <= 10) {
+    numbers.push(currentNumber);
+    currentNumber = currentNumber +1;
+  }
+  return numbers;
 }
+var res = getNumbersToTen();
+console.log('getNumbersToTen()', res);
+
+function getEvenNumbersToTwenty() {
+  var evenNumbers = [];
+  var currentNumber = 2;
+  while (currentNumber <= 20) {
+    evenNumbers.push(currentNumber);
+    currentNumber = currentNumber +2;
+  }
+  return evenNumbers;
+}
+var result = getEvenNumbersToTwenty();
+console.log('getEvenNumbersToTwenty()', result);
+
+function 
