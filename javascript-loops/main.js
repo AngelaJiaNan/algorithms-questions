e52861ffb29304ade7d6c197f0ba19c077fcdf2b
AngelaJiaNan <angelanan12@gmail.com>
2022-05-23 17:21:00 -0700

    finished 3 more problems.

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -43,3 +43,31 @@ function repeatWord(word, times) {
 var word = repeatWord('math', 10);
 console.log('repeatWord()', word);
 
+function logEachCharacter(string) {
+  for (var i =0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
+logEachCharacter('Trevor');
+
+function doubleAll(numbers) {
+  var doubled = [];
+  for (var i=0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+}
+
+var numbers = doubleAll([3,5,7,9]);
+console.log('doubleAll([3,5,7,9])', numbers);
+
+function getKeys(object) {
+  var keys = [];
+  for (key in object) {
+    keys.push(key);
+  }
+  return keys;
+}
+var key = getKeys({ name: 'Angela', title: '24 hours'});
+console.log('getKeys(object)', key);
