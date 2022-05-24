cd325053d7d80b2535abb5ecbc3aea8276ee08d8
AngelaJiaNan <angelanan12@gmail.com>
2022-05-23 18:34:56 -0700

    finished 4 more questions.

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -1,1 +1,10 @@
 /* exported filterOutNulls */
+function filterOutNulls(values) {
+  var results = [];
+  for ( let i = 0; i < values.length; i ++) {
+    if ( values[i] != null) {
+      results.push(values[i]);
+    }
+  }
+  return results;
+}
