8f47cb50db6f2540d6800bb5e3eeabaea1e4268f
AngelaJiaNan <angelanan12@gmail.com>
2022-05-25 19:34:09 -0700

    finished tail.

--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,1 +1,13 @@
 /* exported tail */
+function tail(array) {
+  let result = [];
+  for ( let i = 1; i < array.length; i++) {
+    result.push(array[i]);
+  }
+  return result;
+}
+
+// another Soultion :
+// function tail(array) {
+//   return array.splice(1);
+// }
